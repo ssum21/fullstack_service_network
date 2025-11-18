76f71a32819e481226bad68861ad935d9ca88d23
ssum21 <ssumuss@khu.ac.kr>
2025-11-18 22:39:10 +0900

    03_04_wuserver_wuclient_zmq4 기반 구현

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module fullstack_service_network
 go 1.25.5
 
 require (
+	github.com/alecthomas/gozmq v0.0.0-20140622232202-d1b01a2df6b2 // indirect
 	github.com/go-zeromq/goczmq/v4 v4.2.2 // indirect
 	github.com/go-zeromq/zmq4 v0.17.0 // indirect
+	github.com/pebbe/zmq4 v1.4.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 )
---
+++ lec-05-prg-03-pub-sub-basic-server/4-pub-sub-basic-client.go
@@ -0,0 +1,47 @@
+//
+//  Weather update client.
+//  Connects SUB socket to tcp://localhost:5556
+//  Collects weather updates and finds avg temp in zipcode
+//
+
+package main
+
+import (
+	zmq "github.com/pebbe/zmq4"
+
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	zmq.SetRetryAfterEINTR(true)
+
+	//  Socket to talk to server
+	fmt.Println("Collecting updates from weather server...")
+	subscriber, _ := zmq.NewSocket(zmq.SUB)
+	defer subscriber.Close()
+	subscriber.Connect("tcp://localhost:5556")
+
+	//  Subscribe to zipcode, default is NYC, 10001
+	filter := "10001 "
+	if len(os.Args) > 1 {
+		filter = os.Args[1] + " "
+	}
+	subscriber.SetSubscribe(filter)
+	update_nbr := 0
+	for update_nbr < 20 {
+
+		msg, _ := subscriber.Recv(0)
+
+		if msgs := strings.Fields(msg); len(msgs) >= 3 {
+			temperature, _ := strconv.Atoi(msgs[1])
+
+			fmt.Printf("Receive temperature for zipcode '%s' was %d F\n",
+				strings.TrimSpace(filter), temperature)
+
+			update_nbr++
+		}
+	}
+}
