c631ab28d9c3771bce1ee76005ff83cac056d66b
ssum21 <ssumuss@khu.ac.kr>
2025-11-27 22:10:10 +0900

    09_10_11_lec-05-prg-09-dealer-router-async-server v2 진행

--- lec-05-prg-09-dealer-router-async-server/lec-05-prg-09-dealer-router-async-server.go
+++ lec-05-prg-09-dealer-router-async-server/lec-05-prg-09-dealer-router-async-server.go
@@ -3,29 +3,45 @@ package main
 import (
     "fmt"
     "log"
+    "os"
     "runtime"
     "time"
 
     zmq "github.com/pebbe/zmq4"
 )
 
+func workerRoutine(id int, ctx *zmq.Context) {
+    worker, _ := ctx.NewSocket(zmq.DEALER)
+    worker.Connect("inproc://backend")
+    fmt.Printf("Worker #%d started\n", id)
+
+    for {
+        frames, _ := worker.RecvMessage(0)
+        ident := frames[0]
+        msg := frames[1]
+
+        fmt.Printf("Worker #%d received '%s' from %s\n", id, msg, ident)
+        worker.SendMessage(ident, msg)
+    }
+}
+
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
+    numWorkers := 0
+    fmt.Sscanf(os.Args[1], "%d", &numWorkers)
     ctx, _ := zmq.NewContext()
 
     frontend, _ := ctx.NewSocket(zmq.ROUTER)
     frontend.Bind("tcp://*:5570")
+
     backend, _ := ctx.NewSocket(zmq.DEALER)
     backend.Bind("inproc://backend")
 
-    go func() {
-        for i := 0; i < 3; i++ {
-            time.Sleep(300 * time.Millisecond)
-			worker, _ := ctx.NewSocket(zmq.DEALER)}
-    }()
+    for i := 0; i < numWorkers; i++ {
+        go workerRoutine(i, ctx)
+    }
 
-    fmt.Println("proxy started")
-
+    err := zmq.Proxy(frontend, backend, nil)
     frontend.Close()
     backend.Close()
     ctx.Term()
--- lec-05-prg-09-dealer-router-async-server/lec-05-prg-10-dealer-router-async-client.go
+++ lec-05-prg-09-dealer-router-async-server/lec-05-prg-10-dealer-router-async-client.go
@@ -9,21 +9,30 @@ import (
 )
 
 func main() {
-    clientID := os.Args[1]
-    fmt.Println("Client", clientID, "started")
-
+	clientID := os.Args[1]
     ctx, _ := zmq.NewContext()
     socket, _ := ctx.NewSocket(zmq.DEALER)
     socket.SetIdentity(clientID)
     socket.Connect("tcp://localhost:5570")
+
+    poller := zmq.NewPoller()
+    poller.Add(socket, zmq.POLLIN)
 
     req := 0
     for {
         req++
         msg := fmt.Sprintf("request #%d", req)
         fmt.Println(clientID, "sent:", msg)
-
         socket.Send(msg, 0)
+
+        polled, _ := poller.Poll(1 * time.Second)
+        if len(polled) > 0 {
+            reply, _ := socket.Recv(0)
+            fmt.Printf("%s received: %s\n", clientID, reply)
+        } else {
+            fmt.Println(clientID, "no reply (timeout)")
+        }
+
         time.Sleep(1 * time.Second)
     }
 }
